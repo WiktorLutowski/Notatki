bfaa09168fffed27f6d845f6d9ae785a50625e17
wlutowski <lutowski.wikto@szkola.elektrykplcok.ud.pl>
2026-09-22 12:49:29 +0200

    Work done

--- app/src/main/java/com/example/notatki/MainActivity.java
+++ app/src/main/java/com/example/notatki/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.notatki;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button dodajButton;
+    private EditText tytulEdit;
+    private EditText trescEdit;
+    private ListView lista;
+
+    private  ArrayList<String> notatki = new ArrayList<String>();
+    private ArrayAdapter<String> adapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,32 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        dodajButton = findViewById(R.id.dodaj);
+        tytulEdit = findViewById(R.id.tytul);
+        trescEdit = findViewById(R.id.tresc);
+        lista = findViewById(R.id.lista);
+
+        notatki.add("Tytuł \n treść");
+
+        adapter = new ArrayAdapter<String>(this, R.layout.listviewitem, notatki);
+
+        lista.setAdapter(adapter);
+
+        dodajButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tekst = tytulEdit.getText().toString();
+
+                tekst += "\n";
+
+                tekst += trescEdit.getText().toString();
+
+                notatki.add(tekst);
+
+                adapter.notifyDataSetChanged();
+            }
         });
     }
 }
